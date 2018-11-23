4658726446517467ac326fce6ae41b3d8691828a
mvenkatn <mvenkatn@DIN73003018.corp.capgemini.com>
2018-11-23 12:35:06 +0530

    first message

---
+++ day-Demo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,19 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello good afternoon");
+		
+		greetings();
+	}
+	
+	public static void greetings()
+	{
+		System.out.println("welcome to git");
+	}
+
+}
